9198efc09290893e8273863c62fb76958a8381f5
Ali Amir Khawaja <80505091+aliamir1402@users.noreply.github.com>
2024-03-09 14:53:14 +0500

    Add files via upload

---
+++ TimeVista-FYP-main/src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home.js";
+import Dashboard from "./components/Dashboard.js";
+import Login from "./components/login.js";
+import Signup from "./components/signUp.js";
+import Analytics from "./components/Analytics.js";
+import GISMAP from "../src/components/GISMap.js";
+import NotFound from "./components/NotFound.js"; // Import your 404 page component
+
+export default function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/extreme-events-map" element={<GISMAP />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
