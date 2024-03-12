d4eeca421e057640fc4df0185a921c279afd9bbc
Ali Amir Khawaja <80505091+aliamir1402@users.noreply.github.com>
2024-03-12 17:39:23 +0500

    Update Maps.js

--- TimeVista-FYP-main/src/components/Maps.js
+++ TimeVista-FYP-main/src/components/Maps.js
@@ -11,7 +11,7 @@ export default function Maps() {
     const map = new maplibregl.Map({
       container: "map-markers",
       style:
-        "https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL",
+        "https://api.maptiler.com/maps/streets/style.json?key=ub6D6mcohLuVpSQqkHI2",
       center: [70.0479, 30.6844],
       zoom: 5,
     });
