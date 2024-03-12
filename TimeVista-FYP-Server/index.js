4630438f64e5f6ba1bfce26d4407687bfdf13ae2
Ali Amir Khawaja <80505091+aliamir1402@users.noreply.github.com>
2024-03-12 18:40:44 +0500

    Update index.js

--- TimeVista-FYP-Server/index.js
+++ TimeVista-FYP-Server/index.js
@@ -3,7 +3,7 @@ const { MongoClient } = require("mongodb");
 const cors = require("cors");
 
 const app = express();
-const PORT = const PORT = process.env.PORT || 5000; // Use process.env.PORT or default to 5000
+const PORT = process.env.PORT || 5000; // Use process.env.PORT or default to 5000
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
