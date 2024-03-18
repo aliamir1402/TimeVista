5976c8af2fa6ddb63a5d7fd3df53ca43b3b08d18
Ali Amir Khawaja <aliamirkhawaja1@gmail.com>
2024-03-19 02:34:38 +0500

    Update

--- TimeVista-FYP-main/tailwind.config.js
+++ TimeVista-FYP-main/tailwind.config.js
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("flowbite/plugin")],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
