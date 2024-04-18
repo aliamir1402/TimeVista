37a29cf276c85ec7fd09856adb50cdfdca1eebfb
Ali Amir Khawaja <aliamirkhawaja1@gmail.com>
2024-04-18 11:54:10 +0500

    Blog Update

--- TimeVista-FYP-main/src/components/articles.js
+++ TimeVista-FYP-main/src/components/articles.js
@@ -3,6 +3,7 @@ import Header from "./header";
 import Footer from "./footer";
 import BlogImg from "../components/images/blog.png";
 import RightArrow from "../components/images/icons8-right-button-96.png";
+import { Button } from "flowbite-react";
 
 export default function Articles() {
   const [button1, setButton1] = useState(0);
@@ -10,6 +11,7 @@ export default function Articles() {
   const [button3, setButton3] = useState(0);
   const [button4, setButton4] = useState(0);
   const [button5, setButton5] = useState(0);
+  const items = ["A", "B", "C"];
   return (
     <>
       <div className="">
@@ -94,51 +96,55 @@ export default function Articles() {
       </div>
 
       <div className="flex_box m-12">
-        <div
-          className="flex_item ml-2 mr-2"
-          style={{ border: "1px solid black" }}
-        >
-          <div
-            style={{
-              borderRadius: "15px 15px 0px 0px",
-              boxShadow: "0px 0px 10px 0px #e9e9e9",
-            }}
-          >
-            <img
-              src={BlogImg}
-              alt=""
-              width="35%"
-              height="35%"
-              style={{ borderRadius: "15px 15px 0px 0px" }}
-            />
-          </div>
-          <div className="flex mt-4">
-            <div className="flex justify-left items-center blog-item-1">
-              Features
-            </div>
-            <div className="flex justify-end items-center ml-2 blog-item-2">
-              Nov 9, 2021
-            </div>
-          </div>
-          <div className="title-item-blog">Blog Title</div>
-          <div className="flex justify-left item-center">
-            <div className="flex justify-center items-center">
+        {items.map((item, index) => (
+          <div className="m-2 flex_item" style={{ width: "20vw" }}>
+            <div className="">
               <img
-                src={RightArrow}
-                alt="author"
-                height={40}
-                width={40}
+                className=""
+                src={BlogImg}
+                alt=""
+                width="100%"
+                height="100%"
                 style={{
-                  borderRadius: "15px",
+                  borderRadius: "15px 15px 0px 0px",
+                  boxShadow: "0px 0px 10px 0px #e9e9e9",
                 }}
               />
             </div>
-            <div className="flex justify-center items-center">Author Name</div>
+            <div className="flex mt-4">
+              <div className="flex justify-left items-center blog-item-1">
+                Features
+              </div>
+              <div className="flex justify-end items-center ml-2 blog-item-2">
+                Nov 9, 2021
+              </div>
+            </div>
+            <div className="title-item-blog">Blog Title</div>
+            <div className="flex justify-left item-center">
+              <div className="flex justify-center items-center">
+                <img
+                  src={RightArrow}
+                  alt="author"
+                  height={40}
+                  width={40}
+                  style={{
+                    borderRadius: "15px",
+                  }}
+                />
+              </div>
+              <div className="flex justify-center items-center">
+                Author Name
+              </div>
+            </div>
           </div>
-        </div>
-
-        
+        ))}
       </div>
+      <div className="flex justify-center items-center m-12 mt-4 p-4">
+        <button className="bg-gray-800 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded-xl">
+          Load More...
+        </button>
+      </div>
+
       <div className="">
         <Footer></Footer>
       </div>
