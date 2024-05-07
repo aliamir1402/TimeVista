da5b642d5ac3d54eb786b914cd3a11da16129fe1
Ali Amir Khawaja <80505091+aliamir1402@users.noreply.github.com>
2024-05-07 22:58:46 +0500

    Add files via upload

--- TimeVista-FYP-main/src/components/charts/SimpleLineChart.js
+++ TimeVista-FYP-main/src/components/charts/SimpleLineChart.js
@@ -6,6 +6,7 @@ import {
   YAxis,
   Tooltip,
   Legend,
+  ReferenceLine,
   ResponsiveContainer,
 } from "recharts";
 
@@ -13,18 +14,26 @@ export default function SimpleLineChart(props) {
   const { Data, Type } = props;
 
   return (
-    <div className="temp rounded-lg shadow p-4 md:p-6 divvBox">
+    <div className="shadow p-4 md:p-6 divvBox" style={{ width: "60vw" }}>
       <h2 className="text-center mb-4 text-2xl flex justify-center items-center">
         Historical Trend of Crop {Type}
       </h2>
       <ResponsiveContainer width="100%" height={400}>
-        <LineChart width={700} height={350} data={props.Data[props.index]}>
+        <LineChart width={700} height={380} data={props.Data[props.index]}>
           <XAxis
             dataKey="x"
             name="Year"
             label={{ value: "Year", position: "center", offset: 0 }}
             tickMargin={20}
           />
+          <ReferenceLine
+            x={2020}
+            stroke="red"
+            label="Future Estimates"
+            labelBackgroundColor="lightgrey"
+            position="top"
+          />
+
           <YAxis label={{ value: Type, angle: -90, position: "insideLeft" }} />
           <Tooltip
             cursor={{ strokeDasharray: "3 3" }}
@@ -35,8 +44,8 @@ export default function SimpleLineChart(props) {
               boxShadow: "0px 0px 5px 0px wheat",
             }}
           />
-          <Legend />
-          <Line type="monotone" dataKey="y" stroke="#8884d8" strokeWidth={2} />
+          
+          <Line type="monotone" dataKey={props.Type} stroke="#8884d8" strokeWidth={2} />
         </LineChart>
       </ResponsiveContainer>
     </div>
